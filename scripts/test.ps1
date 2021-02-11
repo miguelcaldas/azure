5109587f153c418e87ec10ced25b483ef194c444
Miguel Caldas <miguelcaldas@users.noreply.github.com>
2021-02-11 02:18:05 +0000

    Update test.ps1

--- scripts/test.ps1
+++ scripts/test.ps1
@@ -1,6 +1,7 @@
 param([string] $name)
 
-$output = 'Hello {0}. The username is {1}, the password is {2}.' -f $name, ${Env:UserName}, ${Env:Password}
+$output = 'Hello {0}. The username is {1}, the password is {2}.' -f $name, ${Env:SASToken}, ${Env:Password}
 Write-Output $output
+azcopy
 $DeploymentScriptOutputs = @{}
 $DeploymentScriptOutputs['text'] = $output
