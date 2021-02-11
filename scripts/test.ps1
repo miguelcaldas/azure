1e73b432ae2d6c487c6845f45bb1d9ee9fa63882
Miguel Caldas <miguelcaldas@hotmail.com>
2021-02-11 20:53:04 +0000

    Update test.ps1

--- scripts/test.ps1
+++ scripts/test.ps1
@@ -1,7 +1,12 @@
 param([string] $name)
 
 $output = 'Hello {0}. The username is {1}, the password is {2}.' -f $name, ${Env:SASToken}, ${Env:Password}
+Invoke-WebRequest -Uri https://mceus2sa.blob.core.windows.net/tools/linux/azcopy -OutFile azcopy -UseBasicParsing
+chmod 755 azcopy
+
 Write-Output $output
-azcopy
+
+./azcopy copy "${Env:Source}" "${Env:Target}?${Env:SASToken}" --from-to=BlobBlob --blob-type=PageBlob --put-md5
+
 $DeploymentScriptOutputs = @{}
 $DeploymentScriptOutputs['text'] = $output
