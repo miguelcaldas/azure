72ef26c4182a0d87d324e05333c7d704d708c356
Miguel Caldas <miguelcaldas@users.noreply.github.com>
2021-02-11 00:08:30 +0000

    Update test.ps1

--- scripts/test.ps1
+++ scripts/test.ps1
@@ -1,1 +1,6 @@
-#
+param([string] $name)
+
+$output = 'Hello {0}. The username is {1}, the password is {2}.' -f $name, ${Env:UserName}, ${Env:Password}
+Write-Output $output
+$DeploymentScriptOutputs = @{}
+$DeploymentScriptOutputs['text'] = $output
