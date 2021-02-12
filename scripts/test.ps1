74b60634f9420fb1ca0ab99d8472b7360b84a8b4
Miguel Caldas <miguelcaldas@users.noreply.github.com>
2021-02-12 00:25:26 +0000

    Update test.ps1

--- scripts/test.ps1
+++ scripts/test.ps1
@@ -1,15 +1,23 @@
 param([string] $name)
 
-$output = 'Hello {0}. The username is {1}, the password is {2}.' -f $name, ${Env:Target}, ${Env:SASToken}
+$output = 'Hello {0}. The Source is {1}, the Target is {2}, the SASToken is {3}.' -f $name, ${Env:Source}, ${Env:Target}, ${Env:SASToken}
 Write-Output $output
 
 Invoke-WebRequest -Uri https://mceus2sa.blob.core.windows.net/tools/linux/azcopy -OutFile azcopy -UseBasicParsing
 chmod 755 azcopy
 
-
-
+Write-Output "./azcopy copy \"${Env:Source}\" \"${Env:Target}?${Env:SASToken}\" --from-to=BlobBlob --blob-type=PageBlob'
 ./azcopy copy "${Env:Source}" "${Env:Target}?${Env:SASToken}" --from-to=BlobBlob --blob-type=PageBlob
-Write-Output './azcopy copy "${Env:Source}" "${Env:Target}?${Env:SASToken}" --from-to=BlobBlob --blob-type=PageBlob'
+
+for ($num = 1; $num -le 1000; $num++) {
+    $sc = (Invoke-WebRequest -Uri "${Env:Target}?${Env:SASToken}" -Method Head -SkipHttpErrorCheck).StatusCode
+    Write-Output $sc
+    if ($sc -ne 200) {
+		Start-Sleep -Seconds 10
+	} else {
+		$num= 1000
+	}
+}
 
 $DeploymentScriptOutputs = @{}
 $DeploymentScriptOutputs['text'] = $output
