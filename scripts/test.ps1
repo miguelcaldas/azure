cce16ad091c246b9272099aa44bf730780741dc9
Miguel Caldas <miguelcaldas@users.noreply.github.com>
2021-02-13 04:23:19 +0000

    Update test.ps1

--- scripts/test.ps1
+++ scripts/test.ps1
@@ -1,13 +1,19 @@
-param([string] $name)
+param(
+    [string] $name,
+    [string] [Parameter(Mandatory=$true)] $sourceUri,
+    [string] [Parameter(Mandatory=$true)] $targetUri
+)
 
-$output = 'Hello {0}. The Source is {1}, the Target is {2}, the SASToken is {3}.' -f $name, ${Env:Source}, ${Env:Target}, ${Env:SASToken}
+$output = 'Hello {0}. The Source is {1}, the Target is {2}, the SASToken is {3}.' -f $name, $sourceUri, $targetUri, ${Env:SASToken}
 Write-Output $output
 
 Invoke-WebRequest -Uri https://mceus2sa.blob.core.windows.net/tools/linux/azcopy -OutFile azcopy -UseBasicParsing
 chmod 755 azcopy
 
-Write-Output "./azcopy copy ""${Env:Source}"" ""${Env:Target}?${Env:SASToken}"" --from-to=BlobBlob --blob-type=PageBlob"
-./azcopy copy "${Env:Source}" "${Env:Target}?${Env:SASToken}" --from-to=BlobBlob --blob-type=PageBlob
+Write-Output "./azcopy copy ""$sourceUri"" ""$targetUri"" --from-to=BlobBlob --blob-type=PageBlob"
+./azcopy copy "$sourceUri" "$targetUri" --from-to=BlobBlob --blob-type=PageBlob
+
+Start-Sleep -Seconds 120
 
 $DeploymentScriptOutputs = @{}
 $DeploymentScriptOutputs['text'] = $output
