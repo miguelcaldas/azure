e14a0b90085aadd5dafd1d691857669440db9733
Miguel Caldas <miguelcaldas@users.noreply.github.com>
2021-02-13 05:03:12 +0000

    Update test.ps1

--- scripts/test.ps1
+++ scripts/test.ps1
@@ -1,22 +1,23 @@
 param(
     [string] $name,
     [string] [Parameter(Mandatory=$true)] $sourceUri,
-    [string] [Parameter(Mandatory=$true)] $targetUri
+    [string] [Parameter(Mandatory=$true)] $targetUri,
+    [string] [Parameter(Mandatory=$true)] $targetSasToken
 )
 
-$output = 'Hello {0}. The Source is {1}, the Target is {2}, the SASToken is {3}.' -f $name, $sourceUri, $targetUri, ${Env:SASToken}
+$output = 'Hello {0}. The Source is {1}, the Target is {2}, the TargetSasToken is {3}.' -f $name, $sourceUri, $targetUri, $targetSasToken
 Write-Output $output
 
 Invoke-WebRequest -Uri https://mceus2sa.blob.core.windows.net/tools/linux/azcopy -OutFile azcopy -UseBasicParsing
 chmod 755 azcopy
 
-Write-Output "./azcopy copy ""$sourceUri"" ""$targetUri"" --from-to=BlobBlob --blob-type=PageBlob"
-./azcopy copy "$sourceUri" "$targetUri" --from-to=BlobBlob --blob-type=PageBlob
+Write-Output "./azcopy copy ""$sourceUri"" ""$targetUri?$targetSasToken"" --from-to=BlobBlob --blob-type=PageBlob"
+./azcopy copy "$sourceUri" "$targetUri?$targetSasToken" --from-to=BlobBlob --blob-type=PageBlob
 
 # Start-Sleep -Seconds 120
 for ($num = 1; $num -le 1000; $num++) {
     $sc = (Invoke-WebRequest -Uri "$targetUri" -Method Head -SkipHttpErrorCheck).StatusCode
-    Write-Output "HTTP Status  $sc"
+    Write-Output "HTTP Status $sc"
     if ($sc -ne 200) {
         Write-Output "Waiting 10 seconds..."
         Start-Sleep -Seconds 10
